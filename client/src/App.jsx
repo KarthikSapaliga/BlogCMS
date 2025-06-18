6c2596cadc232cdf7008bd73036526e08c145328
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-18 23:55:25 +0530

    include Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-    return <div>App</div>;
+    return (
+        <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {/* NAVBAR */}
+            <Navbar />
+            {/* BREADCRUMB */}
+            {/* INTORDUCTION */}
+            {/* FEATURED POSTS */}
+            {/* POST LIST */}
+        </div>
+    );
 }
 
 export default App;
