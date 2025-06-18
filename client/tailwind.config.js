aa35e9e39082207d53adddbc4ffca4aa67d5bff0
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-18 23:53:27 +0530

    added the color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#aa22ff",
+
+                light1: "#ffffff",
+                light2: "#f6f5fa",
+                light3: "#e8e7ed",
+
+                dark1: "#767b8e",
+                dark2: "#5b6274",
+                dark3: "#2b2933",
+
+                green: "#499243",
+                orange: "#de8702",
+                red: "#ff7877",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
