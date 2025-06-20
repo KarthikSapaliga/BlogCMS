a61ecc3284ab4ed056e506a11c6ea36e24982635
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-20 14:54:23 +0530

    setup the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "@/components/Navbar"
+
+import Login from "@/pages/auth/Login.jsx";
+import Register from "@/pages/auth/Register.jsx";
+import Home from "@/pages/home/Home.jsx";
+import PostList from "@/pages/posts/PostList.jsx";
+import SinglePost from "@/pages/posts/SinglePost";
+import Write from "@/pages/write/Write";
 
 function App() {
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-            {/* NAVBAR */}
             <Navbar />
-            {/* BREADCRUMB */}
-            {/* INTORDUCTION */}
-            {/* FEATURED POSTS */}
-            {/* POST LIST */}
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/posts" element={<PostList />} />
+                    <Route path="/:slug" element={<SinglePost />} />
+                    <Route path="/write" element={<Write />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
