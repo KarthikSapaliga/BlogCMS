e2a4aa8dbe2cc3366b5b8fcaa65dd05883ea5658
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-20 15:16:14 +0530

    fix: wrap app in BrowserRouter to resolve Link context error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,8 @@ import Write from "@/pages/write/Write";
 function App() {
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-            <Navbar />
-
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/posts" element={<PostList />} />
