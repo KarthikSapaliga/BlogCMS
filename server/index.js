9b5decb46649af5e11e9c3fc1c96d058c23826f2
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-06 10:28:19 +0530

    chore(index): set cors origin to http://localhost:5173

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
 app.get("/", (req, res) => {
-    res.send(`
-    <div style="text-align:center; margin-top:20px;">
-        <h1>Server is running</h1>
-    </div>    
-    `);
+    console.log(">> check server");
+    return res.status(200).json({ message: "server is running" });
 });
 
 app.listen(PORT, () => {
